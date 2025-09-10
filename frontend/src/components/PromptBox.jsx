0a894e1e725b92328127cc78596030e1754d6125
nikhilyt01 <nikhil0808yt@gmail.com>
2025-09-10 23:13:06 +0530

    .removed

--- frontend/src/components/PromptBox.jsx
+++ frontend/src/components/PromptBox.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function PromptBox({prompt}) {
   return (
-    <div className=' p-4 bg-green-100 text-gray-700  text-justify tracking-tight mb-2 mt-2'>{prompt}</div>
+    <div className=' p-4 bg-green-100 text-gray-700 rounded-xl  font-extrabold tracking-tight mb-2 mt-2'>{prompt}</div>
   )
 }
 
