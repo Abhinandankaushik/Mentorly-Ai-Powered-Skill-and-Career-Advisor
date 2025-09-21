4857981f6bcc08ca331057ce2fb23cf48e93f7e1
nikhilyt01 <nikhil0808yt@gmail.com>
2025-09-21 23:42:07 +0530

    bit responsive

--- frontend/src/components/Home/HeroSection.jsx
+++ frontend/src/components/Home/HeroSection.jsx
@@ -1,15 +1,16 @@
 
+
 function HeroSection() {
   return (
-    <div className='w-full h-130 flex justify-between items-center bg-white hero'>
+    <div className='w-full h-130 flex justify-center  md:justify-between items-center bg-white hero'>
       <div className='flex flex-col h-120 heroSection bg-white justify-center items-start p-10'>
-        <div className='text-8xl font-bold text-black'>From <span className='text-orange-400'>Confusion</span></div>
-        <div className='text-8xl font-bold text-orange-400'>To <span className='text-black'>Clarity</span></div>
-        <div className='text-8xl font-bold text-black'>Mento<span className='text-orange-400'>r</span>ly</div>
-        <div className='text-xl text-black'>AI powered <span className='text-orange-400'>Career</span> & <span className='text-orange-400'>Skill</span> advisor</div>
+        <div className='text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-black'>From <span className='text-orange-400'>Confusion</span></div>
+        <div className='text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-orange-400'>To <span className='text-black'>Clarity</span></div>
+        <div className='text-4xl sm:text-5xl md:text-6xl lg:text-8xl font-bold text-black'>Mento<span className='text-orange-400'>r</span>ly</div>
+        <div className='text-sm sm:text-base md:text-lg lg:text-xl text-black'>AI powered <span className='text-orange-400'>Career</span> & <span className='text-orange-400'>Skill</span> advisor</div>
       </div>
-      <div className='h-120 heroSection object-contain heroImg'>
-        <img src="/HeroSection.jpeg" alt="" className=' w-full h-130'/>
+      <div className='h-120 heroSection object-contain heroImg hidden lg:block'>
+        <img src="../public/HeroSection.jpeg" alt="" className='w-full h-130'/>
       </div>
     </div>
   )
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -6,13 +7,13 @@ const NavBar = () => {
   return (
     <div className=' h-16 top-0 w-full bg-gray-100 sticky navbar flex justify-between items-center border-b-black'>
       <div className='w-32 h-18 flex justify-around items-center '>
-        <img src="/weblogo.jpeg" alt="" className='object-cover h-12 w-12'/>
+        <img src="../public/weblogo.jpeg" alt="" className='object-cover h-12 w-12'/>
         <span className='text-black font-bold text-3xl m-4'>Mento<span className='text-orange-500'>r</span>ly</span>
       </div>
       <div className='flex justify-around gap-5 content-center'>
-        <NavLink to={"/"} className={({isActive})=>isActive? "text-orange-500":"text-black"}>Home</NavLink>
-        <NavLink to={"/testimonial"} className={({isActive})=>isActive? "text-orange-500":"text-black"}>Testimonial</NavLink>
-        <NavLink to={"/aboutUs"} className={({isActive})=>isActive? "text-orange-500":"text-black"}>About us</NavLink>
+        <div className={" hidden md:block"}> <NavLink to={"/"} className={({isActive})=>isActive? "text-orange-500":"text-black "}>Home</NavLink></div>
+        <div className={" hidden md:block"}><NavLink to={"/testimonial"} className={({isActive})=>isActive? "text-orange-500":"text-black"}>Testimonial</NavLink></div>
+        <div className={" hidden md:block"}><NavLink to={"/aboutUs"} className={({isActive})=>isActive? "text-orange-500":"text-black"}>About us</NavLink></div>
       </div>
       <div>
         <button className='btn1'>SignUp</button>
