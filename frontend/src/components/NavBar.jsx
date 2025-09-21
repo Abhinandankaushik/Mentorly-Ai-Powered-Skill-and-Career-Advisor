3f24ba6d88a01e2c887925008fae96275e4e39fb
abhinandan kaushik <abhinandankaushik2004@gmail.com>
2025-09-21 21:27:39 +0530

    import updates

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-import { motion } from 'framer-motion';
 import { NavLink } from 'react-router-dom';
 const NavBar = () => {
   const [isHovered, setIsHovered] = useState(false);
@@ -7,7 +6,7 @@ const NavBar = () => {
   return (
     <div className=' h-16 top-0 w-full bg-gray-100 sticky navbar flex justify-between items-center border-b-black'>
       <div className='w-32 h-18 flex justify-around items-center '>
-        <img src="../public/weblogo.jpeg" alt="" className='object-cover h-12 w-12'/>
+        <img src="/weblogo.jpeg" alt="" className='object-cover h-12 w-12'/>
         <span className='text-black font-bold text-3xl m-4'>Mento<span className='text-orange-500'>r</span>ly</span>
       </div>
       <div className='flex justify-around gap-5 content-center'>
